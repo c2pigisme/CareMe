2102db6a2007527fbf6bf534e95722c3c0e8f5d5
Kok boon <gumestudio@gmail.com>
2013-04-20 15:08:54 +0800

    Added in Login Form

--- app/controller/UIController.js
+++ app/controller/UIController.js
@@ -18,7 +18,8 @@ Ext.define('CareMe.controller.UIController', {
 
     config: {
         refs: {
-            mainView: 'mainview'
+            mainView: 'mainview',
+            loginPanel: 'loginpanel'
         },
 
         control: {
@@ -33,6 +34,9 @@ Ext.define('CareMe.controller.UIController', {
             },
             "mainview button[text='History']": {
                 tap: 'onHistoryButtonTap'
+            },
+            "loginpanel button[itemId='logInButton']": {
+                tap: 'onLogInButtonTap'
             }
         }
     },
@@ -67,6 +71,78 @@ Ext.define('CareMe.controller.UIController', {
 
         history = Ext.create('CareMe.view.HistoryPanel', {title: 'History'});
         me.getMainView().push(history);
+
+    },
+
+    onLogInButtonTap: function(button, e, eOpts) {
+
+        var _this = this, loginPanel, mainView, 
+        usernameField, passwordField, 
+        username, password, label;
+
+        loginPanel = _this.getLoginPanel();
+        mainView = _this.getMainView();
+
+        usernameField = loginPanel.down('#userNameTextField');
+        passwordField = loginPanel.down('#passwordTextField');
+        label = loginPanel.down('#signInFailedLabel');
+
+        username = usernameField.getValue();
+        password = passwordField.getValue();
+        label.hide();
+
+        console.log('=====' + loginPanel);
+        console.log('=====' + mainView);
+        console.log('username : ' + username);
+        console.log('password: ' + password);
+
+        var task = Ext.create('Ext.util.DelayedTask', function () {
+            label.setHtml('');
+            _this.onSignIn(username, password);
+            usernameField.setValue('');
+            passwordField.setValue('');
+        });
+        task.delay(500);
+
+
+    },
+
+    getSlideLeftTransition: function() {
+        return { type: 'slide', direction: 'left' };
+
+    },
+
+    loginFailure: function() {
+        var loginPanel, _this = this;
+        loginPanel.showSignInFailedMessage(message);
+        loginPanel.setMasked(false);
+
+    },
+
+    loginSuccess: function() {
+
+    },
+
+    onSignIn: function(username, password) {
+        var _this = this,
+            loginPanel = _this.getLoginPanel();
+
+        console.log('-- onSignIn() --');
+
+        if (username.length === 0 || password.length === 0) {
+            console.log(' onSignIn() - login failure!');
+            loginPanel.showSignInFailedMessage('Please enter your username and password.');
+            loginPanel.setMasked(false);
+            return;
+
+        } else {
+
+            console.log('onSignIn() - Signed in.');
+            loginPanel = _this.getLoginPanel();
+            mainView = _this.getMainView();
+            loginPanel.setMasked(false);
+            Ext.Viewport.animateActiveItem(mainView, _this.getSlideLeftTransition());
+        }
 
     }
 
